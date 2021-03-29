656cf1ab78f6093c56e4dd93ca49efa70e590058
Alireza382 <alirezasalimi382@gmail.com>
2021-03-29 17:21:31 +0430

    Create Domain Model

---
+++ ShtUrl.Domain/Interfaces/IShooteenUrlRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShtUrl.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShtUrl.Domain.Interfaces
+{
+    public interface IShooteenUrlRepository
+    {
+
+        public IEnumerable<ShorteenUrl> GetShoorteens();
+
+
+        public IEnumerable<ShorteenUrl> GetShorteenUrlsByOriginals(string OriginalUrl);
+
+        public string GetOrginalUrlByShortUrl(string shortUrl);
+
+
+    }
+}
---
+++ ShtUrl.Domain/Models/ShorteenUrl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ShtUrl.Domain.Models
+{
+    [Table(name:"ShorteenUrl")]
+    public  class ShorteenUrl
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string OriginalUrl { get; set; }
+
+        [Required]
+        
+        public string ShortUrl { get; set; }
+    }
+}
